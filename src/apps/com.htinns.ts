2e67389e7752989ee76f263cfab59171d93dd1e8
Rzk <11779765+rensss@users.noreply.github.com>
2025-12-03 10:43:51 +0800

    修复活动ID路径和重置匹配设置

--- src/apps/com.htinns.ts
+++ src/apps/com.htinns.ts
@@ -27,7 +27,7 @@ export default defineGkdApp({
       resetMatch: 'match',
       rules: [
         {
-          activityIds: 'com.htinns.reactnative.v2.RNContainerActivity',
+          activityIds: '.reactnative.v2.RNContainerActivity',
           matches: ['[text="立即签到"]'],
         },
       ],
@@ -38,7 +38,7 @@ export default defineGkdApp({
       fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
-      resetMatch: 'app',
+      resetMatch: 'activity',
       rules: [
         {
           activityIds: 'com.huazhu.main.RnMainActivity',
