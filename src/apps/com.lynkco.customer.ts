1c35a3f9cd0332752ceef4c995381c05dfa38c0d
Rzk <11779765+rensss@users.noreply.github.com>
2026-07-20 11:50:10 +0800

    fix(lynk): 支持两步签到流程

--- src/apps/com.lynkco.customer.ts
+++ src/apps/com.lynkco.customer.ts
@@ -8,12 +8,23 @@ export default defineGkdApp({
       key: 1,
       name: '签到有礼',
       fastQuery: true,
-      matchTime: 10000,
+      matchTime: 15000,
+      forcedTime: 10000,
       actionMaximum: 1,
       resetMatch: 'activity',
       rules: [
         {
-          matches: ['[text="签到有礼"]'],
+          key: 0,
+          name: '进入签到详情',
+          matches: '[text="签到有礼"]',
+        },
+        {
+          key: 1,
+          name: '立即签到',
+          preKeys: [0],
+          activityIds:
+            'com.geely.lynkco.weex.core.activity.WeexAppHostActivity',
+          matches: '[text="立即签到"]',
         },
       ],
     },
